4d7c53d102b97cfeb6547755e5066fe4dfbd4529
SIST <SIST@DESKTOP-CUHMJ4P>
2023-09-11 15:40:54 +0900

    2222

--- Chapter01_Project1/src/MainClass.java
+++ Chapter01_Project1/src/MainClass.java
@@ -1,13 +1,12 @@
 
 public class MainClass {
 
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("github");
 		System.out.println("github");
 		System.out.println("github");
-
-
 	}
 
 }
