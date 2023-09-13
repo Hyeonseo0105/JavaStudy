bb7b95ad6acb77da3cd23d11523edd6211c1cf62
SIST <SIST@DESKTOP-CUHMJ4P>
2023-09-13 17:51:36 +0900

    2222

---
+++ 연산자문제/src/문제1.java
@@ -0,0 +1,159 @@
+import java.util.Scanner; //13번,14번
+
+public class 문제1 {
+    //13번
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*Scanner scan=new Scanner(System.in);
+		System.out.println("세개 정수 입력(1 2 3):");
+		int a=scan.nextInt();
+		int b=scan.nextInt();
+		int c=scan.nextInt();
+		System.out.println(a*b+c);*/
+	
+	//14번
+		Scanner scan=new Scanner(System.in);
+		System.out.println("두개 정수 입력(10 20):");
+		int a=scan.nextInt();
+		int b=scan.nextInt();
+		System.out.println(a%b);
+		
+	
+		
+				
+		/*int a;
+		int b;
+		a=3;
+		b=a+5;
+		System.out.println("a의 값:"+a);
+		System.out.println("b의 값:"+b);
+		
+		int a,b;
+		a=6;
+		b=2;
+		System.out.printf("덧셈 연산 결과:%d\n",a+b);
+		System.out.printf("뺄셈 연산 결과:%d\n",a-b);
+		System.out.printf("곱셈 연산 결과:%d\n",a*b);
+		System.out.printf("나누기 연산 결과:%d\n",a/b);
+		System.out.printf("나머지 연산 결과:%d\n",a%b);
+		
+		int num1=10;
+		System.out.println(num1);
+		num1++;
+		System.out.println(num1);
+		++num1;
+		System.out.println(num1);
+		--num1;
+		System.out.println(num1);
+		num1--;
+		System.out.println(num1);
+		
+		int num1=10, num2=10;
+		int a,b;
+		a=++num1;
+		b=num2++;
+		System.out.printf("%d, %d \n",b,num2);*/
+		
+		/*int num1=10, num2=10;
+		int a,b;
+		num1=num1+1;
+		a=num1;
+		System.out.println(a);
+		System.out.println(num1);
+		b=num2;
+		num2=num2+1;
+		System.out.println(b);
+		System.out.println(num2);
+		
+		int num1=10, num2=10;
+		System.out.println(++num1);
+		System.out.println(num1);
+		System.out.println(num2++);
+		System.out.println(num2);
+		
+		int num1=2, num2=7;
+		int num3, num4;
+		num1++;
+		num3=--num1;
+		--num2;
+		num4=num2++;
+		System.out.println(num3);
+		System.out.println(num4);
+		
+		int i=0, re=0;
+		i=3;
+		re=++i;
+		System.out.println("re:"+re+",i:"+i);
+		i=3;
+		re=i++;
+		System.out.println("re:"+re+",i:"+i);
+		
+		//12번
+		/*
+		 import java.util.Scanner;
+		 
+		public class 문제1 {
+			public static void main(String[] args) {
+				TODO
+				//키보드 입력값을 읽어온다
+				Scanner scan=new Scanner(System.in);
+				System.out.print("첫번째 정수 입력:")
+				int a=scan.nextInt();
+				System.out.print("두번째 정수 입력:")
+				int b=scan.nextInt();
+				
+				System.out.print("정수 두개 입력(10 20):");
+				int a=scan.nextInt();
+				int b=scan.nextInt();
+				System.out.println(a-b);
+				System.out.println(a*b);
+				
+				
+		int a=(int)(Math.random()*100)+1;
+		int b=(int)(Math.random()*100)+1;
+		System.out.println("a="+a+",b="+b);
+		System.out.println("뺄셈:"+(a-b));
+		System.out.println("곱셈:"+(a*b));
+		
+		//13번
+		int c=(int)(Math.random()*10)+1;
+		int d=(int)(Math.random()*10)+1;
+		int D=(int)(Math.random()*10)+1;
+		System.out.println("c="+c+",d="+d+",D="+D);
+		System.out.println("c*d+D="+c*d+D);
+		
+		//14번
+		int A=(int)(Math.random()*100)+1;
+		int B=(int)(Math.random()*100)+1;
+		System.out.println("A="+A+",B="+B);
+		System.out.println("A%B="+(A%B));
+		
+		//15번
+		int a=5;
+		int b=5;
+		//int a=(int)(Math.random()*100)+1;
+		//int b=(int)(Math.random()*100)+1; 
+		
+		boolean aCheck=(a==b);
+		System.out.println("a="+a+",b="+b);
+		System.out.println("a와 b가 같은가요?"+aCheck);
+		System.out.println((a==b)?"같다":"다르다");
+		
+		aCheck=(a>b);
+		System.out.println("a가 b보다 큰가요?"+aCheck);
+		System.out.println((a>b)?"네":"아니오");
+		
+		aCheck=(a<b);
+		System.out.println("a가 b보다 작은가요?"+aCheck);
+		System.out.println((a<b)?"네":"아니오");
+		
+		aCheck=(a==b)&&(a>3);
+		System.out.println("a가 b와 같으면서 3보다 큰가요?"+aCheck);
+		System.out.println(((a==b)&&(a>3))?"네":"아니오");
+		
+		aCheck=(a!=50);
+		System.out.println("a가 50이 아닌가요?"+aCheck);
+		System.out.println((a!=50)?"네":"아니오");*/
+	}
+
+}
