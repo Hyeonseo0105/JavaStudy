fe91f2ed3873c7955ec8f73c271542c2588a6c47
SIST <SIST@DESKTOP-CUHMJ4P>
2023-09-19 16:39:37 +0900

    2222

---
+++ 제어문문제/src/반복문문제.java
@@ -0,0 +1,123 @@
+import java.util.Scanner;
+public class 반복문문제 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//1번
+//		int sum=0;
+//		for(int i=1;i<=100;i++)
+//		{
+//			if(i%2==0)
+//				sum+=i;
+//		}
+//				System.out.println("2+4+...+100의 합:"+sum);
+		
+		
+		//2번
+//		for(int i=1;i<=50;i++)
+//		{
+//			if(i%5==0)
+//				System.out.print(i+" ");
+//		}
+		
+		
+		//3번
+//		for(char i='B';i<='N';i+=2)
+//			System.out.print(i+" ");
+		
+		
+		//4번
+//		Scanner scan=new Scanner(System.in);
+//		System.out.print("정수입력:");
+//		int a=scan.nextInt();
+//		int sum=0;
+//		for(int i=1;i<=a;i++)
+//		{
+//			sum+=i;
+//		}
+//		System.out.println("1~"+a+"까지의 합:"+sum);
+		
+		
+		//5번
+//		for(int i=1;i<=30;i++)
+//		{
+//			if(i%2==0)
+//				System.out.print(i+" ");
+//			if(i%6==0)
+//				System.out.println();
+//		}
+		
+		
+		//6번
+//		int sum=0;
+//		for(int i=1;i<=10;i++)
+//		{
+//			if(i%2==0)
+//				sum=-i;
+//			else
+//				sum=i;
+//			
+//		}
+//		System.out.println("합:"+sum);
+		
+		
+		//7번
+//		for(int i=1;i<=10;i++)
+//		{
+//			if(i%3==0)
+//				continue;
+//			else
+//				System.out.print(i+" ");
+//		}
+		
+		
+		//8번
+//		int a7=0,sum=0;
+//		for(int i=100;i<=999;i++)
+//		{
+//			if(i%7==0)
+//			{
+//				a7++;
+//				sum+=i;
+//			}
+//		}
+//		System.out.println("7의 배수 갯수:"+a7);
+//		System.out.println("7의 배수 합:"+sum);
+		
+		
+		//9번
+//		int a4=0,sum=0;
+//		for(int i=100;i<=999;i++)
+//		{
+//			if(i%4!=0)
+//			{
+//				a4++;
+//				sum+=i;
+//			}
+//			
+//		}
+//		System.out.println("갯수:"+a4);
+//		System.out.println("합:"+sum);
+		
+		
+		//10번
+//		int even=0, odd=0;
+//		for(int i=1;i<=30;i++)
+//		{
+//			if(i%2==0)
+//				even+=i;
+//			else
+//				odd+=i;
+//		}
+//		System.out.println("짝수합:"+even);
+//		System.out.println("홀수합:"+odd);
+		
+		
+		//11번
+//		for(int i=1;i<=100;i++)
+//		{
+//			if(i%3==0 && i%5==0)
+//				System.out.print(i+" ");
+//		}
+	}
+}
